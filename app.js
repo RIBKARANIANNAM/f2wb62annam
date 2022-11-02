0ee157d9ae2d48fc49064fea05c60105adb33d4a
RIBKARANIANNAM <aribkarani2022@gmail.com>
2022-11-01 23:41:51 -0500

    created selector

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var brandRouter = require('./routes/brand');
-
+var gridbuildRouter = require('./routes/gridbuild');
 var app = express();
 
 // view engine setup
@@ -22,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/brand',brandRouter )
-
+app.use('/brand',brandRouter );
+app.use('/gridbuild',gridbuildRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
