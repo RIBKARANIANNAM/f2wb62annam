7361ff5de23649817dcb72bade76bf387bd5bcaf
RIBKARANIANNAM <aribkarani2022@gmail.com>
2022-11-01 23:58:26 -0500

    changes added

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var brandRouter = require('./routes/brand');
 var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
+
+
 var app = express();
 
 // view engine setup
@@ -24,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/brand',brandRouter );
 app.use('/gridbuild',gridbuildRouter);
+app.use('/selector', selectorRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/gridbuild.js
+++ routes/gridbuild.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('gridbuild', { title: 'Grid Build' });
+  let query = req.query
+  console.log(`rows ${query.rows}`)
+  console.log(`cols ${query.cols}`)
+  var rows=Number(query.rows)
+  var cols=Number(query.cols)
+  res.render('gridbuild', { title: 'Grid Build',query: query });
 });
 
 module.exports = router;
